bfce96775ea461c184be184808358cf61ac410c1
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-06-23 14:33:15 -0700

    Set up scoket on the server

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const app = express();
-// const http = require("http").Server(app);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const PORT = process.env.PORT || 3000;
 
-// // Middleware
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
+io.on("connection", function(socket) {
 
-// app.use(express.static(__dirname + "/public"));
+    socket.on("user_join", function(data) {
+        this.username = data;
+        socket.broadcast.emit("user_join", data);
+    });
+
+    socket.on("chat_message", function(data) {
+        data.username = this.username;
+        socket.broadcast.emit("chat_message", data);
+    });
+
+    socket.on("disconnect", function(data) {
+        socket.broadcast.emit("user_leave", this.username);
+    });
+});
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
